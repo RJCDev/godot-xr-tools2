f7109ae33555e2edded0fa4f2748338dde72cdcb
Ryan Crowder <89208776+RJCDev@users.noreply.github.com>
2026-04-10 14:34:10 -0500

    Revert "Update xrt2_collision_hand.gd"
    This reverts commit 39701d5f052fa4498a2c22fd36eef85cc631cbfd.

--- hands/xrt2_collision_hand.gd
+++ hands/xrt2_collision_hand.gd
@@ -55,8 +55,6 @@ signal input_float_changed(action_name: String, value: float)
 signal input_vector2_changed(action_name: String, vector: Vector2)
 #endregion
 
-@export var enabled := true
-
 ## Modes for collision hand
 enum CollisionHandMode {
 	## Hand is disabled and must be moved externally
@@ -600,7 +598,7 @@ func _ready():
 
 # Handle physics processing
 func _physics_process(delta):
-	if Engine.is_editor_hint() or not enabled:
+	if Engine.is_editor_hint():
 		return
 
 	var parent_transform : Transform3D = Transform3D()
@@ -610,6 +608,7 @@ func _physics_process(delta):
 
 	# Handle DISABLED.
 	if mode == CollisionHandMode.DISABLED:
+		freeze = true
 		_was_parent_basis = parent_transform.basis
 		return
 
@@ -683,9 +682,9 @@ func _physics_process(delta):
 	_was_parent_basis = parent_transform.basis
 	_last_tracked_transform = target
 
-# Handle processing
+
 func _process(_delta):
-	if Engine.is_editor_hint() or not enabled:
+	if Engine.is_editor_hint():
 		return
 	
 	var target : Transform3D = get_tracked_transform()
@@ -726,6 +725,7 @@ func _process(_delta):
 		_force_teleport = false
 		
 		self.reset_physics_interpolation()
+		
 		
 		return
 		
